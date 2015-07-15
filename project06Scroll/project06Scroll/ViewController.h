08aa76beb9d500b973ad5f2566fd274a242d42c1
kyoungchin <kcseo.acm@gmail.com>
2015-07-15 21:00:11 +0900

    init project06scroll

---
+++ project06Scroll/project06Scroll/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  project06Scroll
+//
+//  Created by Kyoungchin Seo on 2015. 7. 15..
+//  Copyright (c) 2015년 nhnnext. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ project06Scroll/project06Scroll/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  project06Scroll
+//
+//  Created by Kyoungchin Seo on 2015. 7. 15..
+//  Copyright (c) 2015년 nhnnext. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
