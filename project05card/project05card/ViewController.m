b01a7bad80b13c9d1ad5473dba9ac55420670f4c
kyoungchin <kcseo.acm@gmail.com>
2015-07-15 19:00:33 +0900

    implement functions to display card deck images on the viewcontroller

--- project05card/project05card/ViewController.m
+++ project05card/project05card/ViewController.m
@@ -25,7 +25,27 @@
     deck = [[SolitaireDeck alloc]init];
     [deck ShuffleCards];
     [deck DisplayCards];
+    
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    //NSString *fileName;
+    for (int i=0; i<8; i++) {
+        for (int j=0; j<[[deck.CardDeck objectAtIndex:i]count]; j++) {
+            NSString *fileName= [NSString stringWithFormat:@"%@.png",[[deck.CardDeck objectAtIndex:i] objectAtIndex:j]];
+            UIImageView *cardImage = [[UIImageView alloc]initWithImage:[UIImage imageNamed:fileName]];
+            if (i != 7) {
+                [cardImage setFrame:CGRectMake(i*140+26, j*40+100, 130, 150)];
+            } else {
+                [cardImage setFrame:CGRectMake(j*24+26, 500, 130, 150)];
+            }
+            [self.view addSubview:cardImage];
+        }
+    }
+    
     [deck release];
+    
 }
 
 - (void)didReceiveMemoryWarning {
