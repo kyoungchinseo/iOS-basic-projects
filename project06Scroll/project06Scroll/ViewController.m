b88693a949979656031fa22ae930276f57715f16
kyoungchin <kcseo.acm@gmail.com>
2015-07-20 23:16:06 +0900

    add scrollview & delegate and add all images on scrollview (no height adjustment)

--- project06Scroll/project06Scroll/ViewController.m
+++ project06Scroll/project06Scroll/ViewController.m
@@ -16,7 +16,35 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    //self.scrollView.delegate=self;
     // Do any additional setup after loading the view, typically from a nib.
+    scrollView = [[UIScrollView alloc]initWithFrame:CGRectMake(0, 0,
+                                                               self.view.frame.size.width,
+                                                               self.view.frame.size.height)];
+    
+    //scrollView.pagingEnabled = TRUE;
+    
+    NSInteger numOfImages = 22;
+    for (int i=0;i<numOfImages;i++) {
+        UIImageView *image = [[UIImageView alloc] initWithFrame:
+                              CGRectMake(0,
+                                         self.view.frame.size.height*i,
+                                         self.view.frame.size.width,
+                                         self.view.frame.size.height)];
+        image.image = [UIImage imageNamed:[NSString stringWithFormat:@"%02d.jpg",i+1]];
+        image.contentMode = UIViewContentModeScaleAspectFit;
+        [scrollView addSubview:image];
+    
+    }
+    
+    scrollView.contentSize = CGSizeMake(self.view.frame.size.width,
+                                        self.view.frame.size.height*numOfImages);
+    
+    [self.view addSubview:scrollView];
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
