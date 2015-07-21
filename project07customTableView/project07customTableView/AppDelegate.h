8d7e6f9210cd78266e58ef07c3640d040e7be7ee
kyoungchin <kcseo.acm@gmail.com>
2015-07-21 22:02:00 +0900

    generate new project for custom tableView

---
+++ project07customTableView/project07customTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  project07customTableView
+//
+//  Created by Kyoungchin Seo on 2015. 7. 21..
+//  Copyright (c) 2015년 nhnnext. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ project07customTableView/project07customTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  project07customTableView
+//
+//  Created by Kyoungchin Seo on 2015. 7. 21..
+//  Copyright (c) 2015년 nhnnext. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
